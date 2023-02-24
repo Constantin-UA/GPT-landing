753d9dea827ffda8d413c600be63e05f3461fec1
Constantin-UA <konstantinsh579@gmail.com>
2023-02-24 19:27:31 +0200

    add structur

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Footer, Blog, Possibility, Features, WhatGPT, Header } from './containers';
 import { Navbar, Brand, CTA } from './components';
-
+import './App.css';
 const App = () => {
 	return (
 		<div className="App">
