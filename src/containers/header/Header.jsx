a0e64d5e71c1afedcc9f682ee8a801733adaddc6
Konstantin <konstantinsh579@gmail.com>
2023-02-27 12:57:30 +0200

    add 2 section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './header.css';
-import people from '../../assets/people.png';
 import ai from '../../assets/ai.png';
+import people from '../../assets/people.png';
+import './header.css';
 const Header = () => {
 	return (
-		<div className="gpt__header section_padding" id="home">
+		<div className="gpt__header section__padding" id="home">
 			<div className="gpt__header-content">
 				<h1 className="gradient__text">Let’s Build Something amazing with GPT-3 OpenAI</h1>
 				<p>
--- src/containers/whatGPT/WhatGPT.jsx
+++ src/containers/whatGPT/WhatGPT.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import Features from '../features/Features.jsx';
 import './whatGPT.css';
 
 const WhatGPT = () => {
-	return <div>WhatGPT</div>;
+	return (
+		<div className="gpt__whatgpt section__padding">
+			<div className="gpt__whatgpt-features">
+				<Features />
+			</div>
+			<div className="gpt__whatgpt-heading">
+				<h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+				<p>Explore the Library</p>
+			</div>
+			<div className="gpt__whatgpt-container">
+				<Features />
+				<Features />
+				<Features />
+			</div>
+		</div>
+	);
 };
 
 export default WhatGPT;
